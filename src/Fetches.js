42c974850635840ad453b34486664dbd8b6042eb
Wroppy <wongw@scotscollege.school.nz>
2022-05-22 01:09:57 +1200

    Changed test fetch request to a post request

--- src/Fetches.js
+++ src/Fetches.js
@@ -11,7 +11,7 @@ async function fetchButLikeFetch(path) {
 
     const response = await fetch(`${urlPrefix}${path}`,
             {
-                method: "post",
+                method: "POST",
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(["hi"]),
 
