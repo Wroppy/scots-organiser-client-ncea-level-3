ff3af3df2f8b1f73393ea045d90f39f3fe20ee13
Wroppy <wongw@scotscollege.school.nz>
2022-05-22 17:08:05 +1200

    Added a console.log for debugging reasons

--- src/Fetches.js
+++ src/Fetches.js
@@ -10,6 +10,8 @@ async function serverFetch(path, body, headers) {
     // Sends a post request to the server
     headers = {...headers, "Content-Type": "application/json"};
     console.log(headers)
+    console.log(`Sending request to ${urlPrefix}${path}`)
+
     const response = await fetch(`${urlPrefix}${path}`,
             {
                 method: "POST",
