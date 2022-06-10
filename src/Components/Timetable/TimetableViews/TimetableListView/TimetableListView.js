89f429e645a11b9f0813445aecc846de1e5f8bc3
Weyman Wong <wongw@scotscollege.school.nz>
2022-06-11 01:09:29 +1200

    Added a page when there is no timetable

--- src/Components/Timetable/TimetableViews/TimetableListView/TimetableListView.js
+++ src/Components/Timetable/TimetableViews/TimetableListView/TimetableListView.js
@@ -50,6 +50,7 @@ export default function TimetableListView(props) {
     ]
     const [weekIndex, setWeekIndex] = useState(0);
     const [dayIndex, setDayIndex] = useState(0);
+    console.log(props.timetable);
 
     const changeWeek = (e) => {
         // if week = 0, then newValue = 1
@@ -97,10 +98,16 @@ export default function TimetableListView(props) {
                 <KeyboardArrowRight/>
             </Button>
         </div>
-        <div className="overflow-wrapper">
+        {props.timetable[0].length === 0 ? <div style={{
+            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            padding: "16px"
+        }}>You have not set up your timetable. Press the + button to set your timetable. <br/> 6 Subjects will be needed.</div> : <div className="overflow-wrapper">
             <div className="timetable-week-container">
                 {props.timetable[weekIndex][dayIndex].map((subject, index) => {
-                    console.log(subject);
                     return <div key={index} className="timetable-class-display"
                                 style={subject.subjectName.length !== 0 ? {backgroundColor: props.subjectData[subject.subjectName].background_colour} : {}}>
                         <div className="timetable-class-display-heading">
@@ -112,6 +119,6 @@ export default function TimetableListView(props) {
                     </div>
                 })}
             </div>
-        </div>
+        </div>}
     </div>
 }
